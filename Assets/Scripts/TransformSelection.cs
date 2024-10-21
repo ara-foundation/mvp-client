45cc35c7cfb14a0c5a443e7398cbf66885719228
Medet Ahmetson <milayter@gmail.com>
2024-10-22 01:12:34 +0700

    Remvoe the debugs from transform selection

--- Assets/Scripts/TransformSelection.cs
+++ Assets/Scripts/TransformSelection.cs
@@ -13,19 +13,15 @@ public class TransformSelection : MonoBehaviour
         var activeTab = AraRuntimeEditor_manager.Instance.GetActiveTab();
         if (activeTab == null)
         {
-            Debug.LogWarning("No active tab");
             return;
         }
 
         if (from.CompareTag(ACTProjects.Instance.targetsTag) && enabled)
         {
             var dataGameObjectBehaviour = from.GetComponent<DataGameObjectBehaviour>();
-            Debug.Log($"Target selected is null? {dataGameObjectBehaviour.DataGameObject == null}");
             activeTab.SelectionBehaviour.AddToSelection(dataGameObjectBehaviour.gameObject);
         } else
         {
-            Debug.Log("Target NOT selected");
-
             activeTab.SelectionBehaviour.ClearSelection();
         }
     }
