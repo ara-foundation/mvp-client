fc27129503e9925ccfcf600a6a8b62bd51feae66
Medet Ahmetson <milayter@gmail.com>
2024-10-27 00:44:45 +0700

    Refactoring ACTPart edit

--- Assets/ACT/ACTProject/ACTPart_edit.cs
+++ Assets/ACT/ACTProject/ACTPart_edit.cs
@@ -12,20 +12,29 @@ public class ACTPart_edit : MonoBehaviour
     [SerializeField] private ActivityState ProjectNameState;
     [SerializeField] private GameObject ProjectNameFieldContainer;
 
-    private string _projectName = "";
-
     // Start is called before the first frame update
     void Start()
     {
+        // Show a project name label, hide the project name editing field.
         ToggleProjectNameEditing(edit: false);
     }
 
+    #region ProjectName
+
+    /// <summary>
+    /// Switch between editing the project name and showing it as a label
+    /// </summary>
+    /// <param name="edit"></param>
     void ToggleProjectNameEditing(bool edit)
     {
         ProjectNameState.gameObject.SetActive(!edit);
         ProjectNameFieldContainer.SetActive(edit);
     }
 
+    /// <summary>
+    /// Invoked from Scene. By original design when a user clicks twice on project name label
+    /// </summary>
+    /// <param name="focused"></param>
     public void OnEditProjectName(bool focused)
     {
         if (!focused)
@@ -35,17 +44,6 @@ public class ACTPart_edit : MonoBehaviour
         ProjectNameState.ChangeMode(StateMode.None);
         ToggleProjectNameEditing(edit: true);
         CameraFocus.Instance.SelectTargetThrough(ProjectNameContainer, enabled: true);
-    }
-
-    private void ShowProjectName(string name)
-    {
-        ProjectNameLabel.text = name;
-        ToggleProjectNameEditing(edit: false);
-    }
-
-    public void OnProjectNameEditSelect(string name)
-    {
-        _projectName = name;
     }
 
     /// <summary>
@@ -58,14 +56,18 @@ public class ACTPart_edit : MonoBehaviour
 
         if (!EventSystem.current.alreadySelecting) EventSystem.current.SetSelectedGameObject(null);
 
-        if (Input.GetKeyDown(KeyCode.Escape) || _projectName.Equals(name) || string.IsNullOrEmpty(name))
+        ToggleProjectNameEditing(edit: false);
+
+        if (Input.GetKeyDown(KeyCode.Escape) || ProjectNameLabel.text.Equals(name) || string.IsNullOrEmpty(name))
         {
-            ShowProjectName(ProjectNameLabel.text);
+            // Nothing just cancel
         } else
         {
-            ShowProjectName(name);
+            ProjectNameLabel.text = name;
             // TODO call back the ara tutorial to start showing next part
             Debug.Log("Submit the data (1) check is text changed, (2) call submit to save data in the server");
         }
     }
+
+    #endregion
 }
